f13d11733b444392e3e63a12be42cc177a144f8c
erkenov0999 <erkenov0999@gmail.com>
2025-01-19 14:28:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import manager.InMemoryTaskManager;
+import manager.Status;
+import tasks.Task;
+
+public class Main {
+    public static void main(String[] args) {
+        InMemoryTaskManager manager = new InMemoryTaskManager();
+
+        Task task1 = new Task("Task1", "Taskaaa111", Status.NEW);
+        Task task2 = new Task("Task2", "Taskaaa222", Status.NEW);
+        Task task3 = new Task("Task3", "Taskaaa333", Status.NEW);
+        Task task4 = new Task("Task4", "Taskaaa444", Status.NEW);
+        manager.addNewTask(task1);
+        manager.addNewTask(task2);
+        manager.addNewTask(task3);
+        manager.addNewTask(task4);
+
+        System.out.println(manager.getAllTasks());
+    }
+}
